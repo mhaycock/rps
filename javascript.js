d98effab600fb40864d418af4445fb2cd04911f2
Matthew Haycock <mhaycock@lap-mac-1671.impello.co.uk>
2019-09-20 11:10:46 +0100

    changes for presentation

--- javascript.js
+++ javascript.js
@@ -1,55 +1,56 @@
-function game(){
-
-alert("You are about to take on the computer! Good luck, you're gonna need it...");
-var playerName = prompt("What's your name fool?");
-var computerName = "CPU";
-var playerMove = prompt("What move are you going to play??? rock, paper or scissors? I pity the fool!");
-var computerMove = generateComputerMove();
-console.log(checkTheWinner(computerMove, playerMove, computerName, playerName));
+var heading = document.getElementById("Heading");
+var input = document.getElementById("name");
+var playerName;
+function setName() {
+  playerName = input.value;
 }
 
-function generateComputerMove(){
+function game(playerMove) {
+  var computerName = "CPU";
+  var computerMove = generateComputerMove();
+  heading.innerText = checkTheWinner(
+    computerMove,
+    playerMove,
+    computerName,
+    playerName
+  );
+}
+
+function generateComputerMove() {
   var randomNumber = Math.random();
-  if (randomNumber < 0.33){
+  if (randomNumber < 0.33) {
     return "rock";
-  } else if (randomNumber < 0.66){
+  } else if (randomNumber < 0.66) {
     return "scissors";
-  }else if (randomNumber <= 1){
+  } else if (randomNumber <= 1) {
     return "paper";
   }
 }
 
-function checkTheWinner(move1, move2, name1, name2){
-
-if (move1 === move2) {
-return ("Its a draw!");
-} else if (move2 === "rock"){
-if (move1 === "paper") {
-return (name1 + " wins!");
-
-} else {  return (name2 + " wins");
-}
-} else if (move2 === "paper"){
-if (move1 === "scissors"){
-return (name1 + " wins!");
-
-} else {  return (name2 + " wins")
-}
-} else if (move2 === "scissors"){
-if (move1 === "rock") {
-return (name1 + " wins!");
-
-} else {  return ( name2 +" wins");
-        }
+function checkTheWinner(move1, move2, name1, name2) {
+  if (move1 === move2) {
+    return "Its a draw!";
+  } else if (move2 === "rock") {
+    if (move1 === "paper") {
+      return name1 + " wins!";
+    } else {
+      return name2 + " wins";
     }
+  } else if (move2 === "paper") {
+    if (move1 === "scissors") {
+      return name1 + " wins!";
+    } else {
+      return name2 + " wins";
+    }
+  } else if (move2 === "scissors") {
+    if (move1 === "rock") {
+      return name1 + " wins!";
+    } else {
+      return name2 + " wins";
+    }
+  }
 }
 
-var playAgain = true;
-while(playAgain){
-
-game();
-
-playAgain = confirm("play again?");
-
-}
-
+// change game from prompts to inputs
+// a button for rock, paper and scissors separately
+//player presses rock, computer generates a move, check the winner
